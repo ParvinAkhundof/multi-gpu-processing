0feb03a7e2149bc458d7a7f87c09c788a72f22d7
ParvinAkhundof <pakhundof@gmail.com>
2022-10-30 13:04:16 +0100

    vxcv

--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ import numpy as np
 
 tf_config={
     'cluster': {
-        'worker': ['10.42.0.1:11000', '10.42.0.1:11000']
+        'worker': ['10.42.0.1:11000', '10.42.0.1:11001']
     },
     'task': {'type': 'worker', 'index': 0}
 }
@@ -33,20 +33,17 @@ num_workers = format(strategy.num_replicas_in_sync)
 
 global_batch_size = per_worker_batch_size * num_workers
 print(num_workers)
+
+
  
 
 with strategy.scope():
-  model = tf.keras.Sequential([
-      tf.keras.layers.InputLayer(input_shape=(28, 28)),
-      tf.keras.layers.Reshape(target_shape=(28, 28, 1)),
-      tf.keras.layers.Conv2D(32, 3, activation='relu'),
-      tf.keras.layers.Flatten(),
-      tf.keras.layers.Dense(128, activation='relu'),
-      tf.keras.layers.Dense(10)
-  ])
-  model.compile(
-      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-      optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
-      metrics=['accuracy'])
+    inputs = tf.keras.layers.Input(shape=(10, 1))
+    layer = tf.keras.layers.Dense(5, activation='relu')(inputs)
+    layer = tf.keras.layers.Dense(1)(layer)
 
-print(model)  
+    lstm_stacked = tf.keras.Model(inputs=inputs, outputs=layer)
+    lstm_stacked.compile(optimizer=tf.keras.optimizers.Adam(),loss=tf.keras.losses.MeanSquaredError())
+    lstm_stacked.summary()
+
+print(lstm_stacked)  
