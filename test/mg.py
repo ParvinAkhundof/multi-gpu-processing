a1313842ec4334d0ff67dd72bc9493735b836db1
ParvinAkhundof <pakhundof@gmail.com>
2022-10-30 17:44:59 +0100

    vzcvx

--- test/mg.py
+++ test/mg.py
@@ -7,18 +7,19 @@ import numpy as np
 
 
 
+physical_devices = tf.config.list_physical_devices('GPU')
+print("Num GPUs:", len(physical_devices))
 
+# strategy = tf.distribute.MirroredStrategy(["/cpu:0","/cpu:1"])
 
-strategy = tf.distribute.MirroredStrategy(["/cpu:0","/cpu:1"])
+# per_worker_batch_size = 32
+# num_workers = format(strategy.num_replicas_in_sync)
 
-per_worker_batch_size = 32
-num_workers = format(strategy.num_replicas_in_sync)
+# print(num_workers)
 
-print(num_workers)
+# global_batch_size = per_worker_batch_size * num_workers
 
-global_batch_size = per_worker_batch_size * num_workers
+# with strategy.scope():
+#   mirrored_variable = tf.Variable(1.)
 
-with strategy.scope():
-  mirrored_variable = tf.Variable(1.)
-
-print(mirrored_variable)  
+# print(mirrored_variable)  
