af27c75aba84c177303625d356a649b54ba8196f
ParvinAkhundof <pakhundof@gmail.com>
2022-10-30 12:33:22 +0100

    fsd

--- mw_with_loop/worker.py
+++ mw_with_loop/worker.py
@@ -22,7 +22,7 @@ per_worker_batch_size = 32
 tf_config = json.loads(os.environ['TF_CONFIG'])
 
 strategy = tf.distribute.MultiWorkerMirroredStrategy(
-    communication=tf.distribute.CollectiveCommunication.AUTO,
+    communication=tf.distribute.exrimental.CollectiveCommunication.AUTO,
     cluster_resolver=None
 )
 num_workers = format(strategy.num_replicas_in_sync)
