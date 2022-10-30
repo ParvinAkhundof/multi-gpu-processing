3d80e3de618dc0a83f9de20f08e63624391983e6
ParvinAkhundof <pakhundof@gmail.com>
2022-10-30 13:28:29 +0100

    vxc

--- test/test.py
+++ test/test.py
@@ -8,7 +8,7 @@ import numpy as np
 
 tf_config={
     'cluster': {
-        'worker': ['10.42.0.1:11000', '10.42.0.1:11001']
+        'worker': ['10.42.0.1:8500', '10.42.0.1:8501']
     },
     'task': {'type': 'worker', 'index': 0}
 }
@@ -33,32 +33,20 @@ num_workers = format(strategy.num_replicas_in_sync)
 
 global_batch_size = per_worker_batch_size * num_workers
 print(num_workers)
-
-batch_size = 12
-features_shape = 372, 558, 3
-labels = 10
-sample = tf.random.uniform(features_shape)
-
-def with_shape(t, shape):
-    t = tf.squeeze(t)
-    t.set_shape(shape)
-    return t
-
-ds_train = tf.data.Dataset.from_tensors([sample]).map(lambda s: (s, tf.ones((labels,)))) \
-    .repeat().batch(batch_size).map(lambda s, l: (with_shape(s, (batch_size,) + features_shape),
-                                                    with_shape(l, (batch_size, labels))))
-ds_val = tf.data.Dataset.from_tensors([sample]).map(lambda s: (s, tf.ones((labels,)))) \
-    .repeat().batch(batch_size).take(10).map(
-    lambda s, l: (with_shape(s, (batch_size,) + features_shape), with_shape(l, (batch_size, labels))))
  
 
 with strategy.scope():
-    model = tf.keras.applications.DenseNet121(
-        weights=None, input_shape=features_shape, classes=labels)
-    model.build((batch_size,) + features_shape)
-    model.summary()
-    optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.001)
-    cross_entropy = tf.keras.losses.CategoricalCrossentropy(label_smoothing=0.1)
-    model.compile(optimizer=optimizer, loss=cross_entropy, metrics=["accuracy"])
+  model = tf.keras.Sequential([
+      tf.keras.layers.InputLayer(input_shape=(28, 28)),
+      tf.keras.layers.Reshape(target_shape=(28, 28, 1)),
+      tf.keras.layers.Conv2D(32, 3, activation='relu'),
+      tf.keras.layers.Flatten(),
+      tf.keras.layers.Dense(128, activation='relu'),
+      tf.keras.layers.Dense(10)
+  ])
+  model.compile(
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+      optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+      metrics=['accuracy'])
 
 print(model)  
