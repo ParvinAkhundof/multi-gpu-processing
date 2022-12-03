b55b3df94e2ee8a63a4e7086c7aa8d9abd43839b
Parvin Akhundov <pakhundof@gmail.com>
2022-12-03 19:41:51 +0100

    cleaning

--- multi-gpu/test.py
+++
@@ -1,7 +0,0 @@
-from pynvml import *
-nvmlInit()
-handle = nvmlDeviceGetHandleByIndex(0)
-info = nvmlDeviceGetMemoryInfo(handle)
-print("Total memory:", info.total)
-print("Free memory:", info.free)
-print("Used memory:", info.used)
