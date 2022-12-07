c4ee7ccd831d83d78901c0e0f5680477e50b3318
ParvinAkhundof <pakhundof@gmail.com>
2022-12-07 15:47:28 +0100

    da

--- multi-worker-mnist/config.py
+++ multi-worker-mnist/config.py
@@ -1,7 +1,7 @@
 ip='10.42.0.1'
 
 tf_config={
-    'cluster': {'worker': [ip+':12345','10.42.0.125:12345']},'task': {'type': 'worker', 'index': 0}
+    'cluster': {'worker': [ip+':11000','10.42.0.125:11000']},'task': {'type': 'worker', 'index': 0}
 }
 
 checkpoint_dir = "./ckpt"
