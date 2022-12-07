522ec3673eb8e5c917e11d088994c5d95c0cffa7
ParvinAkhundof <pakhundof@gmail.com>
2022-12-07 16:09:28 +0100

    fds

---
+++ mnist/config.py
@@ -0,0 +1,11 @@
+ip='10.42.0.1'
+
+tf_config={
+    'cluster': {'worker': [ip+':11000','10.42.0.125:11000']},'task': {'type': 'worker', 'index': 0}
+}
+
+checkpoint_dir = "./ckpt"
+
+
+
+
---
+++ mnist/main.py
@@ -0,0 +1,20 @@
+import os
+import json
+
+import tensorflow as tf
+import mnist_setup
+
+per_worker_batch_size = 64
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+strategy = tf.distribute.MultiWorkerMirroredStrategy()
+
+global_batch_size = per_worker_batch_size * 1
+multi_worker_dataset = mnist_setup.mnist_dataset(global_batch_size)
+
+with strategy.scope():
+  # Model building/compiling need to be within `strategy.scope()`.
+  multi_worker_model = mnist_setup.build_and_compile_cnn_model()
+
+
+multi_worker_model.fit(multi_worker_dataset, epochs=3, steps_per_epoch=100)
