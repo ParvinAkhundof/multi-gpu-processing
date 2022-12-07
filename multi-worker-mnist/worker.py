4765007987bdf546b101007a9da8b0e1c97e6e63
ParvinAkhundof <pakhundof@gmail.com>
2022-12-07 16:02:17 +0100

    d

--- multi-worker-mnist/worker.py
+++ multi-worker-mnist/worker.py
@@ -19,7 +19,11 @@ checkpoint_dir = "./ckpt"
 if not os.path.exists(checkpoint_dir):
     os.makedirs(checkpoint_dir)
 
-per_worker_batch_size = 64
+per_worker_batch_size = 32
+
+options = tf.data.Options()
+options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
+
 tf_config = json.loads(os.environ['TF_CONFIG'])
 num_workers = len(tf_config['cluster']['worker'])
 
@@ -27,6 +31,7 @@ strategy = tf.distribute.MultiWorkerMirroredStrategy()
 
 global_batch_size = per_worker_batch_size * num_workers
 multi_worker_dataset = mnist_setup.mnist_dataset(global_batch_size)
+multi_worker_dataset = multi_worker_dataset.with_options(options)
 
 with strategy.scope():
   # Model building/compiling need to be within `strategy.scope()`.
