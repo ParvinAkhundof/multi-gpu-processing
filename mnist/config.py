dcf9e675c94505129af0caf6c58a322e44ca17d6
ParvinAkhundof <pakhundof@gmail.com>
2022-12-07 17:29:44 +0100

    dsf

--- mnist/config.py
+++ mnist/config.py
@@ -1,11 +1,16 @@
 ip='10.42.0.1'
 
+# tf_config={
+#     'cluster': {'worker': [ip+':12345','10.42.0.125:23456']},'task': {'type': 'worker', 'index': 0}
+# }
+
+# checkpoint_dir = "./ckpt"
+
+
+
+
 tf_config={
-    'cluster': {'worker': [ip+':12345','10.42.0.125:23456']},'task': {'type': 'worker', 'index': 0}
+    'cluster': {'worker': [ip+':12345',ip+':23456']},'task': {'type': 'worker', 'index': 0}
 }
 
 checkpoint_dir = "./ckpt"
-
-
-
-
