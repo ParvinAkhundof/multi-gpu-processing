f49f88dc4bdf76dde8d8c77796716c73e9dc83de
Parvin Akhundov <pakhundof@gmail.com>
2022-12-08 15:14:31 +0100

    fsd

--- mnist/config.py
+++ mnist/config.py
@@ -15,6 +15,10 @@ checkpoint_dir = "./ckpt"
 # }
 
 
+# tf_config={
+#     'cluster': {'worker': ['192.168.75.25:12345','192.168.75.27:23456']},'task': {'type': 'worker', 'index': 0}
+# }
+
 tf_config={
-    'cluster': {'worker': ['192.168.75.25:12345','192.168.75.27:23456']},'task': {'type': 'worker', 'index': 0}
+    'cluster': {'worker': ['192.168.75.25:12345']},'task': {'type': 'worker', 'index': 0}
 }
