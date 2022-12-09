622d53d0596bd53b155d5b37015ce570be6b072c
ParvinAkhundof <pakhundof@gmail.com>
2022-12-09 23:14:16 +0100

    fs

--- multi-worker-svhn/worker.py
+++ multi-worker-svhn/worker.py
@@ -9,7 +9,32 @@ import sys
 import time
 import mnist_setup
 
+import socket
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
 tf_config=config.tf_config
+
+index=0
+my_ip=get_ip()
+for x in tf_config['cluster']['worker']:
+  if(x.split(':')[0]==my_ip):
+    break
+  index=index+1
+
+print(index)
+
 tf_config['task']['index'] = int(sys.argv[1])
 
 os.environ['TF_CONFIG']=json.dumps(tf_config)
@@ -34,9 +59,9 @@ strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy(
 print("Number of devices: {}".format(strategy.num_replicas_in_sync))
 
 global_batch_size = per_worker_batch_size * num_workers
-multi_worker_dataset = svhn_setup.svhn_train_dataset(global_batch_size) ##SVHN
+# multi_worker_dataset = svhn_setup.svhn_train_dataset(global_batch_size) ##SVHN
 
-# multi_worker_dataset = mnist_setup.mnist_dataset(global_batch_size)   ##MNIST
+multi_worker_dataset = mnist_setup.mnist_dataset(global_batch_size)   ##MNIST
 
 options = tf.data.Options()
 options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
@@ -45,8 +70,8 @@ multi_worker_dataset = multi_worker_dataset.with_options(options)
 start_time = time.time()
 with strategy.scope():
     
-  multi_worker_model = make_or_restore.make_or_restore_model(checkpoint_dir) ##SVHN
-  # multi_worker_model = mnist_setup.build_and_compile_cnn_model()  ##MNIST
+  # multi_worker_model = make_or_restore.make_or_restore_model(checkpoint_dir) ##SVHN
+  multi_worker_model = mnist_setup.build_and_compile_cnn_model()  ##MNIST
 
 # callbacks = [
     
