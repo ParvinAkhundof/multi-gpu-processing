90b9137b66c94ba6aa3bbbc8430e985ec73c06f2
Parvin Akhundov <pakhundof@gmail.com>
2022-12-09 22:08:59 +0100

    fs

--- multi-worker-svhn/config.py
+++ multi-worker-svhn/config.py
@@ -1,5 +1,14 @@
+# tf_config={
+#     'cluster': {'worker': ['192.168.2.135:12345','192.168.2.140:23456']},'task': {'type': 'worker', 'index': 0}
+# }
+
+
+# tf_config={
+#     'cluster': {'worker': ['192.168.75.25:12345','192.168.75.27:23456']},'task': {'type': 'worker', 'index': 0}
+# }
+
 tf_config={
-    'cluster': {'worker': ['192.168.2.135:12345','192.168.2.140:23456']},'task': {'type': 'worker', 'index': 0}
+    'cluster': {'worker': ['localhost:12345']},'task': {'type': 'worker', 'index': 0}
 }
 
 checkpoint_dir = "./ckpt"
--- multi-worker-svhn/worker.py
+++ multi-worker-svhn/worker.py
@@ -6,17 +6,19 @@ import svhn_setup
 import make_or_restore
 import config
 import sys
+import time
+import mnist_setup
 
 tf_config=config.tf_config
 tf_config['task']['index'] = int(sys.argv[1])
 
 os.environ['TF_CONFIG']=json.dumps(tf_config)
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-checkpoint_dir =config.checkpoint_dir
-if not os.path.exists(checkpoint_dir):
-    os.makedirs(checkpoint_dir)
+# checkpoint_dir =config.checkpoint_dir
+# if not os.path.exists(checkpoint_dir):
+#     os.makedirs(checkpoint_dir)
 
 per_worker_batch_size = 32
 tf_config = json.loads(os.environ['TF_CONFIG'])
@@ -26,19 +28,27 @@ strategy = tf.distribute.MultiWorkerMirroredStrategy()
 print("Number of devices: {}".format(strategy.num_replicas_in_sync))
 
 global_batch_size = per_worker_batch_size * num_workers
-multi_worker_dataset = svhn_setup.svhn_train_dataset(global_batch_size)
+# multi_worker_dataset = svhn_setup.svhn_train_dataset(global_batch_size) ##SVHN
 
+multi_worker_dataset = mnist_setup.mnist_dataset(global_batch_size)   ##MNIST
+
+start_time = time.time()
 with strategy.scope():
     
-  multi_worker_model = make_or_restore.make_or_restore_model(checkpoint_dir)
-print(1)
-callbacks = [
-    
-    keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_dir + "/ckpt" #, save_freq=100
-    ),
-    keras.callbacks.TensorBoard(checkpoint_dir + "/tb/")
-]
-print(2)
+#   multi_worker_model = make_or_restore.make_or_restore_model(checkpoint_dir) ##SVHN
+  multi_worker_model = mnist_setup.build_and_compile_cnn_model()  ##MNIST
 
-multi_worker_model.fit(multi_worker_dataset,callbacks=callbacks,steps_per_epoch=100)
+# callbacks = [
+    
+#     keras.callbacks.ModelCheckpoint(
+#         filepath=checkpoint_dir + "/ckpt" #, save_freq=100
+#     ),
+#     keras.callbacks.TensorBoard(checkpoint_dir + "/tb/")
+# ]
+# multi_worker_model.fit(multi_worker_dataset,callbacks=callbacks)
+multi_worker_model.fit(multi_worker_dataset)
+
+
+elapsed_time = time.time() - start_time
+str_elapsed_time = time.strftime("%H : %M : %S", time.gmtime(elapsed_time))
+print(">> Finished. Time elapsed: {}.".format(str_elapsed_time))
