a67727a3e0de4229a012a9a53b426eee4ade03ab
ParvinAkhundof <pakhundof@gmail.com>
2022-12-09 23:15:22 +0100

    fs

--- multi-worker-svhn/config.py
+++ multi-worker-svhn/config.py
@@ -8,13 +8,13 @@
 # }
 
 
-# tf_config={
-#     'cluster': {'worker': ['192.168.75.27:12345','192.168.75.28:23456','192.168.75.29:34567']},'task': {'type': 'worker', 'index': 0}
-# }
-
-
 tf_config={
-    'cluster': {'worker': ['localhost:12345']},'task': {'type': 'worker', 'index': 0}
+    'cluster': {'worker': ['192.168.75.27:12345','192.168.75.28:23456','192.168.75.29:34567']},'task': {'type': 'worker', 'index': 0}
 }
+
+
+# tf_config={
+#     'cluster': {'worker': ['localhost:12345']},'task': {'type': 'worker', 'index': 0}
+# }
 
 checkpoint_dir = "./ckpt"
