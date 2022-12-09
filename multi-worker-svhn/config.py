7a51a1ff34c61e6daebdc2e6de49ab8a5c009113
ParvinAkhundof <pakhundof@gmail.com>
2022-12-09 23:18:08 +0100

    fs

--- multi-worker-svhn/config.py
+++ multi-worker-svhn/config.py
@@ -3,14 +3,14 @@
 # }
 
 
-# tf_config={
-#     'cluster': {'worker': ['192.168.75.27:12345','192.168.75.28:23456']},'task': {'type': 'worker', 'index': 0}
-# }
-
-
 tf_config={
-    'cluster': {'worker': ['192.168.75.27:12345','192.168.75.28:23456','192.168.75.29:34567']},'task': {'type': 'worker', 'index': 0}
+    'cluster': {'worker': ['192.168.75.27:12345','192.168.75.28:23456']},'task': {'type': 'worker', 'index': 0}
 }
+
+
+# tf_config={
+#     'cluster': {'worker': ['192.168.75.27:12345','192.168.75.28:23456','192.168.75.29:34567']},'task': {'type': 'worker', 'index': 0}
+# }
 
 
 # tf_config={
