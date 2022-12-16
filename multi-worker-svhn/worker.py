62a61ad3d9d4d4af2e590bf415b2ab59182d7c0a
ParvinAkhundof <pakhundof@gmail.com>
2022-12-16 13:57:16 +0100

    gfd

--- multi-worker-svhn/worker.py
+++ multi-worker-svhn/worker.py
@@ -94,9 +94,9 @@ elapsed_time = time.time() - start_time
 str_elapsed_time = time.strftime("%H : %M : %S", time.gmtime(elapsed_time))
 print(">> Finished. Time elapsed: {}.".format(str_elapsed_time))
 
-test_dataset = svhn_setup.svhn_test_dataset()
+# test_dataset = svhn_setup.svhn_test_dataset()  ##SVHN
 
-loss, acc = multi_worker_model.evaluate(test_dataset)
-print("Model accuracy on test data is: {:6.3f}%".format(100 * acc))
+# loss, acc = multi_worker_model.evaluate(test_dataset)
+# print("Model accuracy on test data is: {:6.3f}%".format(100 * acc))
 
 
