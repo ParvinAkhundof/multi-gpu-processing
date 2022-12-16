8bfd6b65d03cf8d0bbbc940fafe63d57b7806f39
ParvinAkhundof <pakhundof@gmail.com>
2022-12-16 13:37:31 +0100

    gdf

--- multi-worker-svhn/worker.py
+++ multi-worker-svhn/worker.py
@@ -43,7 +43,7 @@ for x in tf_config['cluster']['worker']:
 
 os.environ['TF_CONFIG']=json.dumps(tf_config)
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 checkpoint_dir =config.checkpoint_dir
 if not os.path.exists(checkpoint_dir):
