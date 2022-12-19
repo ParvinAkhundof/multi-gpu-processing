14d0909e99dc9f882a016e04647e38546da4aa5b
ParvinAkhundof <pakhundof@gmail.com>
2022-12-20 00:42:24 +0100

    fsd

--- multi-worker/mnist_setup.py
+++ multi-worker/mnist_setup.py
@@ -35,7 +35,10 @@ def mnist_dataset_train(batch_size,index,num_workers):
 
   data_size_start=int(x_train.size/num_workers*index)
   data_size_end=int(x_train.size/num_workers*(index+1))
-
+  print("start")
+  print(data_size_start)
+  print("end")
+  print(data_size_end)
 
   x_train=x_train[data_size_start:data_size_end]
   y_train=y_train[data_size_start:data_size_end]
