cb27e3816588248fe7d1158befc5e712054fe657
ParvinAkhundof <pakhundof@gmail.com>
2022-12-19 22:31:56 +0100

    fs

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 import nvidia_smi
+import time
 
-nvidia_smi.nvmlInit()
+for x in  range(10):
+    nvidia_smi.nvmlInit()
 
-deviceCount = nvidia_smi.nvmlDeviceGetCount()
-print(deviceCount)
-for i in range(deviceCount):
-    handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
-    info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
-    print("Device {}: {}, Memory : ({:.2f}% free): {}(total), {} (free), {} (used)".format(i, nvidia_smi.nvmlDeviceGetName(handle), 100*info.free/info.total, info.total, info.free, info.used))
+    deviceCount = nvidia_smi.nvmlDeviceGetCount()
+    print(deviceCount)
+    for i in range(deviceCount):
+        handle = nvidia_smi.nvmlDeviceGetHandleByIndex(i)
+        info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+        print("Device {}: {}, Memory : ({:.2f}% free): {}(total), {} (free), {} (used)".format(i, nvidia_smi.nvmlDeviceGetName(handle), 100*info.free/info.total, info.total, info.free, info.used))
 
-nvidia_smi.nvmlShutdown()
+    nvidia_smi.nvmlShutdown()
+    time.sleep(1)
 
 # import nvidia_smi
 # nvidia_smi.nvmlInit()
