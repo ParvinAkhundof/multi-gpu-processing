2065ee7705d3e5cd9087c12fadafb4fd009aa5bc
ParvinAkhundof <pakhundof@gmail.com>
2022-12-20 00:10:43 +0100

    fsd

--- multi-worker/worker.py
+++ multi-worker/worker.py
@@ -64,9 +64,9 @@ global_batch_size = per_worker_batch_size * num_workers
 
 multi_worker_dataset = mnist_setup.mnist_dataset_train(global_batch_size)   ##MNIST
 
-# options = tf.data.Options()
-# options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-# multi_worker_dataset = multi_worker_dataset.with_options(options)
+options = tf.data.Options()
+options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
+multi_worker_dataset = multi_worker_dataset.with_options(options)
 
 
 with strategy.scope():
