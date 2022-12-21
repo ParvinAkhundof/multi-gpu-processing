0a3d69469622b2178eabfb2dbec0dd96a2654c34
ParvinAkhundof <pakhundof@gmail.com>
2022-12-22 00:32:46 +0100

    fsd

--- multi-worker/mnist_setup.py
+++ multi-worker/mnist_setup.py
@@ -55,25 +55,21 @@ def mnist_dataset_test(batch_size,index,num_workers):
 
 def build_and_compile_cnn_model():
   model = keras.Sequential()
-  model.add(keras.Input(shape=(28, 28, 1)))  
-  # model.add(keras.layers.Conv2D(32, 3, activation="relu"))
-  # model.add(keras.layers.Flatten())
-  # model.add(keras.layers.Dense(128, activation="relu"))
-  # model.add(keras.layers.Dense(10, activation='softmax'))
-
+  model.add(keras.Input(shape=(28, 28, 1))) 
   model.add(keras.layers.Conv2D(32, (3,3), activation="relu"))
-  # model.add(keras.layers.Conv2D(32, (3,3), activation="relu"))
-  # model.add(keras.layers.MaxPooling2D(2,2))
-  # model.add(keras.layers.Dropout(0.3))
-  # model.add(keras.layers.Conv2D(64, (3,3), activation="relu"))
-  # model.add(keras.layers.Conv2D(64, (3,3), activation="relu"))
-  # model.add(keras.layers.MaxPooling2D(2,2))
-  # model.add(keras.layers.Dropout(0.3))
+  model.add(keras.layers.Conv2D(32, (3,3), activation="relu"))
+  model.add(keras.layers.MaxPooling2D(2,2))
+  model.add(keras.layers.Dropout(0.3))
+  model.add(keras.layers.Conv2D(64, (3,3), activation="relu"))
+  model.add(keras.layers.Conv2D(64, (3,3), activation="relu"))
+  model.add(keras.layers.MaxPooling2D(2,2))
+  model.add(keras.layers.Dropout(0.3))
   model.add(keras.layers.Flatten())
 
-  # model.add(keras.layers.Dense(512, activation="relu"))
-  # model.add(keras.layers.Dropout(0.3))
+  model.add(keras.layers.Dense(512, activation="relu"))
+  model.add(keras.layers.Dropout(0.3))
   model.add(keras.layers.Dense(10, activation='softmax'))
+
 
   model.summary()
   model.compile(
