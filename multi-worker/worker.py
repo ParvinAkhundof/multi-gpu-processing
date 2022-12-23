39379902e3558363c2b2b17fb66d9ed5987b487f
ParvinAkhundof <pakhundof@gmail.com>
2022-12-23 13:34:12 +0100

    das

--- multi-worker/worker.py
+++ multi-worker/worker.py
@@ -48,13 +48,14 @@ tf_config = json.loads(os.environ['TF_CONFIG'])
 ##########
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
-  # Restrict TensorFlow to only use the first GPU
   try:
-    tf.config.set_visible_devices(gpus[0], 'GPU')
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
     logical_gpus = tf.config.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
   except RuntimeError as e:
-    # Visible devices must be set before GPUs have been initialized
+    # Memory growth must be set before GPUs have been initialized
     print(e)
 
 #########
